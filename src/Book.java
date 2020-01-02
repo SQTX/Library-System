9c4211d7a78e0af2b1cd4b3e6d50107ab14b3ddc
Jakub SQTX Sitarczyk <skks717@wp.pl>
2020-01-02 20:55:14 +0100

    Library v0.2 (new) -Create class Book.java -Transform Library.java to object (modified) Rename version to "Library v0.2"

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020. SQTX
+ *
+ * Copyright © 2020 SQTX. All rights reserved.
+ */
+
+public class Book {
+    String title;
+    String author;
+    int releaseDate;
+    int pages;
+    String publisher;
+    String isbn;
+}
